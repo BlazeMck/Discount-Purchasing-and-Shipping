98aa4271ffa6508d6fc941d805785607a3cdd5a4
Riley McKinlay <blazemckinlay@gmail.com>
2025-09-09 08:14:13 -0600

    Initial Commit

---
+++ IDiscount.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discount_Purchasing_and_Shipping
+{
+    internal interface IDiscount
+    {
+        // Used to calculate and display each possible total for that object
+        void Calculate();
+        // Used to determine what the discount/shipping rate will be for that object
+        double DetermineRate();
+    }
+}
